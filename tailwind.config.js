463682ffae0e95eb97be4a08a4a42fd8e5a09d07
alzeqri1997 <ahmed1221moh@gmail.com>
2022-09-13 17:26:16 +0300

    change poppins to roboto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,32 @@
-/** @type {import('tailwindcss').Config} */
+/**
+ * @format
+ * @type {import('tailwindcss').Config}
+ */
+
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  mode: "jit",
-  theme: {
-    extend: {
-      colors: {
-        primary: "#243FE2",
-        background:"#06090F",
-        secondary: "#00f6ff",
-        dimWhite: "rgba(255, 255, 255, 0.7)",
-        dimBlue: "rgba(9, 151, 124, 0.1)",
-      },
-      fontFamily: {
-        poppins: ["Roboto", "sans-serif"],
-      },
-    },
-    screens: {
-      xs: "480px",
-      ss: "620px",
-      sm: "768px",
-      md: "1060px",
-      lg: "1200px",
-      xl: "1700px",
-    },
-  },
-  plugins: [],
-};
+	content: ["./index.html", "./src/**/*.{js,jsx}"],
+	mode: "jit",
+	theme: {
+		extend: {
+			colors: {
+				primary: "#243FE2",
+				background: "#06090F",
+				secondary: "#00f6ff",
+				dimWhite: "rgba(255, 255, 255, 0.7)",
+				dimBlue: "rgba(9, 151, 124, 0.1)",
+			},
+			fontFamily: {
+				roboto: ["Roboto", "sans-serif"],
+			},
+		},
+		screens: {
+			xs: "480px",
+			ss: "620px",
+			sm: "768px",
+			md: "1060px",
+			lg: "1200px",
+			xl: "1700px",
+		},
+	},
+	plugins: [],
+}
